c629740a1425951bf3e060814602f290802e9747
TSopon <sopon101n@gmail.com>
2019-12-24 22:14:00 +0700

    add data

---
+++ src/data.js
@@ -0,0 +1,22 @@
+export const shopdata =[{
+    id: 1,
+    name: "โกอ่างข้าวมันไก่ประตูน้ำ",
+    img: "https://lh5.googleusercontent.com/p/AF1QipOFHTPhdE29SPZax2t7UFJK25a9s6jDw0iSm1xb=w408-h306-k-no",
+    detail:  `ร้านข้าวมันได่ยอดฮิต `,
+    address:  `960 962 ถนน เพชรบุรี แขวง มักกะสัน เขตราชเทวี กรุงเทพมหานคร 10400`
+},
+{
+    id: 2,
+    name: "โกอ่างข้าวมันไก่ประตูน้ำ",
+    img: "https://lh5.googleusercontent.com/p/AF1QipNB-YDqRtart_4kuDlQIimlRj1fklUQvJ2IHruR=w408-h272-k-no",
+    detail:  `เนื้อย่างเจ้าดัง`,
+    address:  `ซอย สุขุมวิท 49 ซอย พร้อมมิตร แขวง คลองเตยเหนือ เขตวัฒนา กรุงเทพมหานคร 10110`
+},
+{
+    id: 3,
+    name: "โกอ่างข้าวมันไก่ประตูน้ำ",
+    img: "https://lh5.googleusercontent.com/p/AF1QipOlWY2KHgSNmUbxhWtoTtYU-k9qLQifdrkDumg-=w408-h306-k-no",
+    detail:  `อาหารเช้าสุดอร่อย`,
+    address:  `72 ถนน เจริญกรุง แขวง วังบูรพาภิรมย์ เขตพระนคร กรุงเทพมหานคร 10200`
+},
+]
