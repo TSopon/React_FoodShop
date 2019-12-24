ec38d7946e7182e25a25ed95c86efaaafe2ae597
TSopon <sopon101n@gmail.com>
2019-12-24 22:21:01 +0700

    add shoplist / store

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Navbar from './Components/Navbar/Navbar'
+import ShopList from './Components/ShopList'
 
 function App() {
   return (
     <div class="container">
       <Navbar/>
+      <ShopList/>
     </div>
   );
 }
