6a0a55e3c64b949a3e9bf0d07b2934fd35f6a248
TSopon <sopon101n@gmail.com>
2019-12-24 22:04:02 +0700

    nav comp

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 import Navbar from './Components/Navbar/Navbar'
 
 function App() {
   return (
-    <div>
+    <div class="container">
       <Navbar/>
     </div>
   );
